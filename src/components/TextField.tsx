5e28adb688084a30c57d24e49792106dbd6491b6
Theo Gernke <theogernke@icloud.com>
2026-05-16 17:47:43 +0200

    Highlights the entire text when focussed

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, useId } from 'react';
+import { ComponentPropsWithoutRef, FocusEvent, useId } from 'react';
 
 import Icon from '@/components/Icon';
 
@@ -7,13 +7,25 @@ import { TYPOGRAPHY_CONFIGS } from '@/constants/typography-configs.constant';
 export default function ({ name, onChange, placeholder, value }: Pick<ComponentPropsWithoutRef<'input'>, 'name' | 'onChange' | 'placeholder' | 'value'>) {
 	const id = useId();
 
-	const typographyConfig = TYPOGRAPHY_CONFIGS['preset-5'];
+	const handleFocus = (event: FocusEvent<HTMLInputElement>) => {
+		setTimeout(() => event.target.select(), 0);
+	};
 
 	return (
 		<label className='rounded-12 relative flex h-[56px] grow cursor-text items-center gap-200 bg-neutral-800 px-300 hover:bg-neutral-700' htmlFor={id}>
 			<Icon name='search' size={20} />
 
-			<input id={id} className={`${typographyConfig.styles} peer grow font-medium text-neutral-200 outline-none`} name={name} onChange={onChange} placeholder={placeholder} type='text' value={value} />
+			<input
+				id={id}
+				className={`${TYPOGRAPHY_CONFIGS['preset-5'].styles} peer grow font-medium text-neutral-200 outline-none`}
+				name={name}
+				onChange={onChange}
+				onFocus={handleFocus}
+				placeholder={placeholder}
+				type='text'
+				value={value}
+			/>
+
 			<div className='rounded-16 border-neutral-0 absolute -inset-50 hidden border-2 peer-focus:block' />
 		</label>
 	);
