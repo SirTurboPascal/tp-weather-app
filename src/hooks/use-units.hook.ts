d58215918a58d23e07882e8dfb2eeb408279f131
Theo Gernke <theogernke@icloud.com>
2026-06-14 12:25:56 +0200

    Allows for persisting units over sessions

--- src/hooks/use-units.hook.ts
+++ src/hooks/use-units.hook.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 import { Quantity } from '@/model/types/quantity.type';
 import { Unit } from '@/model/types/unit.type';
@@ -11,20 +12,23 @@ type Action = {
 	setUnit: (quantity: Quantity, unit: State['units'][Quantity]) => void;
 };
 
-export const useUnits = create<State & Action>((set, get) => {
-	return {
-		units: {
-			precipitation: 'mm',
-			temperature: 'celsius',
-			wind_speed: 'kmh',
-		},
+export const useUnits = create<State & Action>()(
+	persist(
+		(set, get) => ({
+			units: {
+				precipitation: 'mm',
+				temperature: 'celsius',
+				wind_speed: 'kmh',
+			},
 
-		setUnit: (quantity, unit) => {
-			const { units } = get();
+			setUnit: (quantity, unit) => {
+				const { units } = get();
 
-			set({
-				units: { ...units, [quantity]: unit },
-			});
-		},
-	};
-});
+				set({
+					units: { ...units, [quantity]: unit },
+				});
+			},
+		}),
+		{ name: 'units' },
+	),
+);
